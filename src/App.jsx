ce5bb80a283b3b385a815b77dca4574e812e43bc
ANKESH <ankeshsharma@protonmail.com>
2025-12-03 13:07:48 +0530

    feat: weather api called

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [coordinates, setCoordinates] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
+  navigator.geolocation.getCurrentPosition((position) => {
+    const { latitude, longitude } = position.coords;
+    // Fetch the weather data
+    const API_KEY = import.meta.env.VITE_OPEN_WEATHER_SECRET_KEY;
+    const url = `${import.meta.env.VITE_OPEN_WEATHER_BASE_URL}weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setWeatherData(data))
+      .catch((error) => console.error("error"));
+    setCoordinates({ latitude, longitude });
+  });
+  useEffect(() => {
+    console.log(weatherData);
+  }, [weatherData]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {coordinates && (
+        <>
+          <h1>{coordinates.latitude}</h1>
+          <h1>{coordinates.longitude}</h1>
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
