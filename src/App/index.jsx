78025fdd57c2d8b999d9c9ecfcc50f304b52ea8b
ANKESH <ankeshsharma@protonmail.com>
2026-01-15 14:26:25 +0530

    - Added Ui for current weather component.

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -2,6 +2,7 @@ import Background from "@/App/components/Background";
 import "@/App/index.css";
 
 import { WeatherProvider, ImageProvider } from "./Context";
+import { Body, Card, Footer, Title } from "./components/Card";
 
 function App() {
   return (
@@ -15,17 +16,19 @@ function App() {
               <div className="current-n-glimpse">
                 <div className="search-container"></div>
                 <div className="greeting-container">
-                  <div className="card greeting"></div>
-                  <div className="card hero">
-                    <div className="title">
+                  <Card className="greeting"></Card>
+                  <Card className="hero">
+                    <Title className="head">
                       <p className="main-title">Current Weather</p>
                       <p className="sub-title">6:48 PM</p>
-                    </div>
-                    <div className="body temprature">
+                    </Title>
+
+                    <Body className="temprature">
                       <span className="value">45 &deg; C</span>
                       <span className="description">Clear</span>
-                    </div>
-                    <div className="footer stats">
+                    </Body>
+
+                    <Footer className="stats">
                       <div className="temp-stat">
                         <p className="property">Air Quality</p>
                         <p className="value">255</p>
@@ -46,9 +49,9 @@ function App() {
                         <p className="property">Pressure</p>
                         <p className="value">10</p>
                       </div>
-                    </div>
-                  </div>
-                  <div className="card favorite"></div>
+                    </Footer>
+                  </Card>
+                  <Card className="favorite"></Card>
                 </div>
               </div>
               <div className="history"></div>
