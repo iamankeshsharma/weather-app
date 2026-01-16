60664f58eb8d6b02d2496beb21b1a7f4b3a0b6b4
ANKESH <ankeshsharma@protonmail.com>
2026-01-16 13:38:28 +0530

    fix: logging the resource temprarly.

--- src/shared/config/env.js
+++ src/shared/config/env.js
@@ -1,12 +1,14 @@
-export const imageEnv = {
+const imageEnv = {
   BASE_URL: import.meta.env.VITE_UNSPLASH_BASE_URL,
   ACCESS_KEY: import.meta.env.VITE_UNSPLASH_ACCESS_KEY,
   SECRET_KEY: import.meta.env.VITE_UNSPLASH_SECRET_KEY,
   TIMEOUT: 5000,
 };
 
-export const weatherEnv = {
+const weatherEnv = {
   BASE_URL: import.meta.env.VITE_OPEN_WEATHER_BASE_URL,
   SECRET_KEY: import.meta.env.VITE_OPEN_WEATHER_SECRET_KEY,
   TIMEOUT: 5000,
 };
+
+export { imageEnv, weatherEnv };
